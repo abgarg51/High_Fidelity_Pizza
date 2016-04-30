1535ca58e9e98dc7abc966fb1eb9373f67277140
samiramly <samiramly@users.noreply.github.com>
2016-04-30 15:50:15 -0700

    Update cheese.js

--- cheese.js
+++ cheese.js
@@ -1,16 +1,11 @@
-
-
 (function(){ 
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    };
+
     this.collisionWithEntity = function(myID, otherID, collisionInfo) { 
-        Entities.editEntity(myID, { color: { red: getRandomInt(128,255), green: getRandomInt(128,255), blue: getRandomInt(128,255)} });
+        Entities.editEntity(myID, { color: { red: getRandomInt(128,255), 
+                                            green: getRandomInt(128,255), 
+                                            blue: getRandomInt(128,255)} });
     }; 
 })
-
-Console.log("hello world");
-print("hello world");
-
-Script.stop();
