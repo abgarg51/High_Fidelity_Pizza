39b27e293746be2ffa11d72771f64cd1448a2e23
Abhishek <agarg5@stanford.edu>
2016-04-30 15:46:20 -0700

    Update cheese.js

--- cheese.js
+++ cheese.js
@@ -13,7 +13,7 @@
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-//can edit
+//can afdsdsfsad
     };
     this.collisionWithEntity = function(myID, otherID, collisionInfo) { 
         Entities.editEntity(myID, { color: { red: getRandomInt(128,255), green: getRandomInt(128,255), blue: getRandomInt(128,255)} });
