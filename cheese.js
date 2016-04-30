feb672c7d27435079872fc34e6227f4c26a18906
Abhi Garg <agarg5@stanford.edu>
2016-04-30 16:27:41 -0700

    mouse

--- cheese.js
+++ cheese.js
@@ -1,16 +1,13 @@
 (function(){
-    function change(entityID) {
-        Entities.editEntity(entityID, { angularDamping: 0});
-        Entities.editEntity(entityID, { angularVelocity: { x: 0, y: 60, z: 0} });
-        Entities.editEntity(entityID, { color: { red: 255, green: 100, blue: 220} });
-    }
-    this.enterEntity = function(entityID) {
-        print("enterEntity("+entityID.id+")");
-        change(entityID);
-    };
-    this.leaveEntity = function(entityID) {
-        print("leaveEntity("+entityID.id+")");
-        Entities.editEntity(entityID, { angularDamping: 0.5});
-        Entities.editEntity(entityID, { color: { red: 255, green: 190, blue: 20} });
+    var black = 0;
+  var white = 255;
+  var color = white;  
+    this.mouseMoveOnEntity = function(entityID, mouseEvent) { 
+         if (color == white) {
+         color = black;
+     }else {
+         color = white;
+     }
+        Entities.editEntity(entityID, { color: { red: color, green: color, blue: color} });
     };
 })
