a7a932f4347353b29be15acfc1c67e09b63a5496
Abhi Garg <agarg5@stanford.edu>
2016-04-30 15:25:23 -0700

    spawns cow as test

--- cheese.js
+++ cheese.js
@@ -14,3 +14,39 @@
         Entities.editEntity(entityID, { color: { red: 255, green: 190, blue: 20} });
     };
 })
+
+Console.log("hello world");
+print("hello world");
+
+var cow = Entities.addEntity({
+  type: "Model",
+  modelURL: MODEL_URL,
+  name: "example_cow",
+  position: center,
+  animation: {
+    currentFrame: 278,
+    running: true,
+    url: ANIMATION_URL
+  },
+  dimensions: {
+    x: 0.739,
+    y: 1.613,
+    z: 2.529
+  },
+  dynamic: true,
+  gravity: {
+    x: 0,
+    y: -5,
+    z: 0
+  },
+  lifetime: 3600,
+  shapeType: "box",
+  script: SCRIPT_URL,
+  userData: JSON.stringify({
+    grabbableKey: {
+      grabbable: true
+    }
+  })
+});
+
+Script.stop();
