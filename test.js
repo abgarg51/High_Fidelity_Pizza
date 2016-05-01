82943dd887cbef44e69305fa2c516215e3b872a9
Abhi Garg <agarg5@stanford.edu>
2016-04-30 19:40:16 -0700

    sadf

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-(function(){
-    this.mousePressOnEntity = function(entityID, mouseEvent) { 
+(function() {
+    this.mousePressOnEntity = function(entityID, mouseEvent) {
         print("clicked");
-        
-        Entities.editEntity(entityID, {visible: FALSE});
+
+        Entities.editEntity(entityID, {
+            visible: FALSE
+        });
     };
-    this.mouseReleaseOnEntity = function(entityID, mouseEvent) { 
-    
-        Entities.editEntity(entityID, {visible: TRUE});
+    this.mouseReleaseOnEntity = function(entityID, mouseEvent) {
+
+        Entities.editEntity(entityID, {
+            visible: TRUE
+        });
     };
 });
