e41b1e579595fbcf4eee9b8a7628c9bc8828efd3
Abhi Garg <agarg5@stanford.edu>
2016-04-30 20:09:49 -0700

    asfd

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 print("started");
 (function() {
-    this.mousePressOnEntity = function(entityID, mouseEvent) {
-        print("clicked");
-
-        // Create the sphere properties
-        var pos = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
-        var properties = {
+    var properties = {
             type: "Model",
             modelURL : "https://www.dropbox.com/s/612pgw8woehz5e8/Pizza.fbx?dl=0",
             position: pos,
             name: "new_pizza"
         };
+    this.mousePressOnEntity = function(entityID, mouseEvent) {
+        print("clicked");
+
+        // Create the sphere properties
+        var pos = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
+        
 
         // Add the sphere
         var Ent = Entities.addEntity(properties);
