5dda671aaa4496433dfc838f3cbfc69ef86c1b39
Abhi Garg <agarg5@stanford.edu>
2016-04-30 20:46:44 -0700

    changed link and position

--- test.js
+++ test.js
@@ -3,14 +3,22 @@ print("started");
 
     this.clickDownOnEntity = function(entityID, mouseEvent) {
         print("clicked3");
+        var myProps = Entities.getEntityProperties(entityID);
+        var pizzaPosition = Vec3.sum(myProps.position,{
+            x:0,
+            y:1,
+            z:0,
+        })
 
         // Create the sphere properties
         var pos = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
+
             var properties = {
             type: "Model",
-            modelURL : "https://rawgit.com/abgarg51/High_Fidelity_Pizza/v0/Pizza.fbx",
-            position: pos,
-            name: "new_pizza"
+            modelURL : "atp:/pizza/Pizza.fbx",
+            position: pizzaPosition,
+            name: "new_pizza",
+            dimensions:{x:1,y:1,z:1}
         };
 
         // Add the sphere
