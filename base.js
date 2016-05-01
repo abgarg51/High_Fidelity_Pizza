78767d1a39038935b44c83a5a423be17e74e922e
Abhi Garg <agarg5@stanford.edu>
2016-05-01 00:17:42 -0700

    initial

---
+++ base.js
@@ -0,0 +1,28 @@
+print("started");
+(function() {
+
+    this.collisionWithEntity = function(myID, otherID, collisionInfo) {
+        print("collision");
+        var myProps = Entities.getEntityProperties(myID);
+        var otherProps = Entities.getEntityProperties(otherID);
+        print("name: "+otherProps["name"]);
+        var pizzaPosition = Vec3.sum(myProps.position,{
+            x:0,
+            y:.2,
+            z:0,
+        })
+
+            var properties = {
+            type: "Model",
+            modelURL : "atp:/pizza/Pizza.fbx",
+            position: pizzaPosition,
+            name: "new_pizza",
+            dimensions:{x:0.3549,y:0.0325,z:0.3549}
+        };
+
+        // Add the sphere
+        if (otherProps["name"] === "pizza-base-no-texture")
+            var Ent = Entities.addEntity(properties);
+    };
+
+});   
