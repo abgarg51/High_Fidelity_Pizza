567360e39431d95d43eab42e4a88d179154bd2ec
Abhishek <agarg5@stanford.edu>
2016-04-30 18:30:08 -0700

    Update cheese.js

--- cheese.js
+++ cheese.js
@@ -1,13 +1,26 @@
 (function(){
-    var black = 0;
-  var white = 255;
-  var color = white;  
-    this.mouseMoveOnEntity = function(entityID, mouseEvent) { 
-         if (color == white) {
-         color = black;
-     }else {
-         color = white;
-     }
-        Entities.editEntity(entityID, { color: { red: color, green: color, blue: color} });
+    var change = 12;
+    var red = 100;
+  var blue = 100;
+    this.mousePressOnEntity = function(entityID, mouseEvent) { 
+        red = red - change;
+        blue = blue + change;
+        if (blue >=  255) {
+            blue = 255;
+        }
+        if (red <=  0) {
+            red = 0;
+        }
+        Entities.editEntity(entityID, { color: { red: red, green: 0, blue: blue} });
     };
-})
+    this.mouseReleaseOnEntity = function(entityID, mouseEvent) { 
+    red = red + change;
+      blue = blue - change;
+        if (red >=  255) {
+            red = 255;
+        }
+        if (blue <=  0) {
+            blue = 0;
+        }   
+        Entities.editEntity(entityID, { color: { red: red, green: 0, blue: blue} });
+    };  
