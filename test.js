c19d49d6670f01df7843f94e7cd9b49431e4621a
Abhi Garg <agarg5@stanford.edu>
2016-04-30 20:40:19 -0700

    asdfsaf

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 print("started");
 (function() {
-    var properties = {
-            type: "Model",
-            modelURL : "https://rawgit.com/abgarg51/High_Fidelity_Pizza/v0/Pizza.fbx",
-            position: pos,
-            name: "new_pizza"
-        };
+
     this.clickDownOnEntity = function(entityID, mouseEvent) {
         print("clicked3");
 
         // Create the sphere properties
         var pos = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
-        
+            var properties = {
+            type: "Model",
+            modelURL : "https://rawgit.com/abgarg51/High_Fidelity_Pizza/v0/Pizza.fbx",
+            position: pos,
+            name: "new_pizza"
+        };
 
         // Add the sphere
         var Ent = Entities.addEntity(properties);
