67397b0a94d7abadce615346e3be224e777867fa
Abhi Garg <agarg5@stanford.edu>
2016-04-30 19:57:01 -0700

    sdfs

--- test.js
+++ test.js
@@ -6,13 +6,14 @@ print("started");
         // Create the sphere properties
         var pos = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
         var properties = {
+            type: "Model",
             modelURL : "atp:/pizza/Pizza.fbx",
-            position: pos
+            position: pos,
+            name: "new_pizza"
         };
 
         // Add the sphere
         var Ent = Entities.addEntity(properties);
-        print("Entity added.");;
     };
 
 });
