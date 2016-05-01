92f5e256eed9469ededeccad24ee0aaaa609c446
Abhi Garg <agarg5@stanford.edu>
2016-04-30 19:46:29 -0700

    asfd

--- test.js
+++ test.js
@@ -3,9 +3,16 @@ print("started");
     this.mousePressOnEntity = function(entityID, mouseEvent) {
         print("clicked");
 
-        Entities.editEntity(entityID, {
-            visible: FALSE
-        });
+        // Create the sphere properties
+        var pos = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
+        var properties = {
+            type: "Sphere",
+            position: pos
+        };
+
+        // Add the sphere
+        var Ent = Entities.addEntity(properties);
+        print("Entity added.");;
     };
 
 });
