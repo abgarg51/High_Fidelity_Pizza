ab22c98f57287d569b9d040e583c0691566f1098
Abhishek <agarg5@stanford.edu>
2016-04-30 20:04:33 -0700

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ print("started");
         var pos = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation));
         var properties = {
             type: "Model",
-            modelURL : "atp:/pizza/Pizza.fbx",
+            modelURL : "https://rawgit.com/abgarg51/High_Fidelity_Pizza/v0/Pizza.fbx",
             position: pos,
             name: "new_pizza"
         };
