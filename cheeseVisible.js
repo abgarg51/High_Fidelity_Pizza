b3767d3517c3e5725637ae99ef64355a58bb1338
Abhishek <agarg5@stanford.edu>
2016-04-30 19:18:56 -0700

    Update cheeseVisible.js

--- cheeseVisible.js
+++ cheeseVisible.js
@@ -2,12 +2,15 @@
     var newProperties = {
       visible: FALSE
     };
+    var newProperties2 = {
+      visible: TRUE
+    };
     this.mousePressOnEntity = function(entityID, mouseEvent) { 
         
         Entities.editEntity(entityID, newProperties);
     };
     this.mouseReleaseOnEntity = function(entityID, mouseEvent) { 
     
-        Entities.editEntity(entityID, newProperties);
+        Entities.editEntity(entityID, newProperties2);
     };
 })
